6d41c55d3dd2317d41d27448a406ba6b72ff8ce8
theernis <ernestas.pakstas@gmail.com>
2024-03-17 19:10:17 +0000

    added debug flag to demo.sh

--- demo.sh
+++ demo.sh
@@ -9,16 +9,34 @@ esac
 
 print_help() {
 	echo "options:"
-	echo "-h, --help	show this"
+	echo "-h, --help		show this"
+	echo "-d, --debug=[yYnN]	turn on debug mode"
 }
 
 errors=0
+debug_mode=0
+
+unset_all() {
+	unset debug_mode
+	unset errors
+}
 
 while test $# -gt 0; do
 	case "$1" in
 		-h|--help)
 			print_help
 			errors=$((errors+1))
+			shift
+			;;
+		-d|--debug)
+			debug_mode=1
+			shift
+			;;
+		--debug=*)
+			tmp=$1
+			tmp="${tmp#--debug=}"
+			[[ $tmp =~ [yY] ]] && debug_mode=1 || debug_mode=0
+			unset tmp
 			shift
 			;;
 		*)
@@ -29,8 +47,11 @@ while test $# -gt 0; do
 	esac
 done
 
-if [[ errors != 0 ]]; then
+if [[ $errors != 0 ]]; then
 	echo "exiting"
-	unset errors
+	unset_all
 	exit 0
 fi
+
+[[ $debug_mode != 0 ]] && sleep 10
+unset_all
