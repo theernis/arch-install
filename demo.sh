89f4e156e113b30bd2d7bf3fb3372f6a3735feb4
theernis <ernestas.pakstas@gmail.com>
2024-03-18 15:29:33 +0000

    added disk input flag

--- demo.sh
+++ demo.sh
@@ -11,12 +11,31 @@ print_help() {
 	echo "options:"
 	echo "-h, --help		show this"
 	echo "-d, --debug=[yYnN]	turn on debug mode"
+	echo "--disk=*			set install disk path (example /dev/sdb)"
+}
+
+check_disk() {
+	path=$1
+	if [[ $path != "/dev/sd"? ]]; then
+		echo "$path unexpected disk format"
+		unset path
+		return 1
+	fi
+	if lsblk "$path" > /dev/null 2>&1; then
+		unset path
+	else
+		echo "disk not found"
+		unset path
+		return 1
+	fi
 }
 
 errors=0
 debug_mode=0
+disk_path=""
 
 unset_all() {
+	unset disk_path
 	unset debug_mode
 	unset errors
 }
@@ -36,6 +55,14 @@ while test $# -gt 0; do
 			tmp=$1
 			tmp="${tmp#--debug=}"
 			[[ $tmp =~ [yY] ]] && debug_mode=1 || debug_mode=0
+			unset tmp
+			shift
+			;;
+		--disk=*)
+			tmp=$1
+			tmp="${tmp#--disk=}"
+			check_disk $tmp && tmp=""
+			[[ tmp == "" ]] && errors=$((errors+1))
 			unset tmp
 			shift
 			;;
